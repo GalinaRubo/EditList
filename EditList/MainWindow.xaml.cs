e55f96b5405753df97b5b713b0e518a259990f16
Galina.Rubo <Rubo_eh74@itstepru.onmicrosoft.com>
2022-09-05 13:01:03 +0700

    Добавьте файлы проекта.

---
+++ EditList/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+
+namespace EditList
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			foreach (string q in ReadWriteQuestionsFile.ReadListQuestions())
+				Questions.Items.Add(q);
+
+		}
+
+		private void Extract_TextChanged_1(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private void ComboBox_Selected(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		private void Check_Click_Import(object sender, RoutedEventArgs e)
+		{
+
+			Questions.Text = Questions.Items[10].ToString();
+		}
+
+		private void Check_Click_Save(object sender, RoutedEventArgs e)
+		{
+			var question = this.Extract.Text;
+			int index = 0;
+			foreach (var q in Questions.Items)
+			{
+				if (q == Questions.Text)
+				{ Questions.Items.RemoveAt(index); break; }
+				index++;
+			}
+			Questions.Items.Add(question);
+			Questions.Text = Questions.Items[10].ToString();
+
+		}
+
+		private void Check_Click_Export(object sender, RoutedEventArgs e)
+		{
+			List<string> _questions = new List<string>();
+			foreach (var q in Questions.Items)
+			{
+				_questions.Add(q.ToString());
+			}
+			ReadWriteQuestionsFile.WriteListQuestions(_questions);
+		}
+	}
+
+}
---
+++ EditList/ReadWriteQuestionsFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json;
+using System.IO;
+namespace EditList
+{
+	internal class ReadWriteQuestionsFile
+	{
+		public static List<string> ReadListQuestions()
+		{
+			List<string> listQ = new List<string>();
+			//чтение из файла всей информации по вопросам
+			string readText = File.ReadAllText("QLiterature");
+			//делим строку на подстроки, разделитель '{'и'}' 
+			string[] words = readText.Split(new char[] { '{', '}'});
+			foreach (string q in words)
+			{
+			listQ.Add( q + "}\n{" );			
+			}
+			return listQ;
+		}
+		public static  void WriteListQuestions(List<string> listQ)
+		{
+			FileStream file = new FileStream("QLiterature", FileMode.Truncate);
+			file.Close();
+			string ListQuestions = "";
+			foreach (string question in listQ)
+			{
+				ListQuestions = ListQuestions + question ;
+			}
+				using (StreamWriter fw = File.AppendText("QLiterature"))
+				{
+					fw.WriteLine(ListQuestions);
+				}			
+		}
+	}
+}
